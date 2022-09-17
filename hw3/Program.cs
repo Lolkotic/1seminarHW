0606677b065cea331613730e94659e4f963730a2
Lola <lola@example.com>
2022-09-17 16:21:49 +0300

    hw3

---
+++ hw3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число и я определю чётное ли оно: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)  
+{
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("нет");
+}
