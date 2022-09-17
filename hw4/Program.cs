7db65dbd6d0ad660763a5acd8651aeebfb4ab990
Lola <lola@example.com>
2022-09-17 16:33:55 +0300

    hw4

---
+++ hw4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count<=number)
+{
+    Console.WriteLine(count);
+    count++;
+}
